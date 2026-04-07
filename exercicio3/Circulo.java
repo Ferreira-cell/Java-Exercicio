5e86d152fb9991dd6f35f6b688ec963203832b44
Ferreira-cell <vtinaugusto17@gmail.com>
2026-04-07 20:44:25 -0300

    Add files via upload

---
+++ exercicio3/Circulo.java
@@ -0,0 +1,19 @@
+package com.mycompany.formageometrica;
+
+public class Circulo extends FormaGeometrica {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+}
---
+++ exercicio3/FormaGeometrica.java
@@ -0,0 +1,9 @@
+package com.mycompany.formageometrica;
+
+    public abstract class FormaGeometrica {
+ 
+    public abstract double calcularArea();
+    public abstract double calcularPerimetro();
+}
+
+
